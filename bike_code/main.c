aefbbf47329a5d212175bee04853cc2c7fccbe7e
Bernard Chen <bernardchen@berkeley.edu>
2019-11-19 17:37:07 -0800

    Working on turn lighting state machine
    Deleted some of the debug code we had previously. Added init code for things needed, like app_timers, accelerations from the mpu9250. Currently debugging the turn_state state machine. Timer callback not getting called :(

--- bike_code/main.c
+++ bike_code/main.c
@@ -21,9 +21,13 @@
 #include "nrfx_gpiote.h"
 #include "nrfx_saadc.h"
 
-// bucker includes
+// buckler includes
 #include "buckler.h"
 #include "display.h"
+#include "mpu9250.h"
+
+// our includes
+#include "gpio.h"
 
 // macros for accelerometer
 #define pi acos(-1.0)
@@ -37,6 +41,12 @@
 #define X_CHANNEL 0
 #define Y_CHANNEL 1
 #define Z_CHANNEL 2
+
+// Create timer
+APP_TIMER_DEF(adv_timer);
+
+// I2C manager
+NRF_TWI_MNGR_DEF(twi_mngr_instance, 5, 0);
 
 // Bucker LED array
 static uint8_t LEDS[3] = {BUCKLER_LED0, BUCKLER_LED1, BUCKLER_LED2};
@@ -94,6 +104,22 @@ void init_accelerometer() {
   APP_ERROR_CHECK(error_code);
 
   printf("Accelerometer initialized!\n");
+}
+
+void init_mpu9250() {
+  ret_code_t error_code = NRF_SUCCESS;
+
+  // initialize i2c master (two wire interface)
+  nrf_drv_twi_config_t i2c_config = NRF_DRV_TWI_DEFAULT_CONFIG;
+  i2c_config.scl = BUCKLER_SENSORS_SCL;
+  i2c_config.sda = BUCKLER_SENSORS_SDA;
+  i2c_config.frequency = NRF_TWIM_FREQ_100K;
+  error_code = nrf_twi_mngr_init(&twi_mngr_instance, &i2c_config);
+  APP_ERROR_CHECK(error_code);
+
+  // initialize MPU-9250 driver
+  mpu9250_init(&twi_mngr_instance);
+  printf("MPU-9250 initialized\n");
 }
 
 void init_SD_logging() {
@@ -171,6 +197,26 @@ void init_buckler_LEDs() {
   }
 }
 
+void init_button0() {
+  gpio_config(28, 0);
+}
+
+// Callback function for timer
+// turn_time_on used for turn signal state machine
+uint32_t turn_time_on = 0;
+uint32_t button_press_time = 0;
+void timer_callback() {
+  printf("Timer fired\n");
+  turn_time_on++;
+  button_press_time++;
+}
+
+void init_app_timer() {
+  app_timer_init();
+  app_timer_create(&adv_timer, APP_TIMER_MODE_REPEATED, (app_timer_timeout_handler_t) timer_callback);
+  app_timer_start(adv_timer, APP_TIMER_TICKS(1000), NULL); // 1000 milliseconds
+}
+
 void sample_accel(double* x_acc, double* y_acc, double* z_acc) {
   nrf_saadc_value_t x_val = sample_value(X_CHANNEL);
   nrf_saadc_value_t y_val = sample_value(Y_CHANNEL);
@@ -186,6 +232,13 @@ void sample_accel(double* x_acc, double* y_acc, double* z_acc) {
   //double phi = atan(z_acc / pow(pow(y_acc, 2) + pow(x_acc, 2), 0.5)) * radToDeg;
 }
 
+void sample_9250_accelerometer(float* x_axis, float* y_axis, float* z_axis) {
+  mpu9250_measurement_t accelerometer_measurement = mpu9250_read_accelerometer();
+  *x_axis = accelerometer_measurement.x_axis;
+  *y_axis = accelerometer_measurement.y_axis;
+  *z_axis = accelerometer_measurement.z_axis;
+}
+
 typedef enum {
   RIGHT,
   LEFT,
@@ -197,49 +250,50 @@ int main (void) {
   // Initialization code
   init_RTT();
   init_accelerometer();
+  init_mpu9250();
   init_buckler_LEDs();
+  init_app_timer();
+  init_button0();
   printf("Buckler initialized!\n");
 
-  /*
-  simple_logger_log("----- STARTING RUN -----\n");
-  init_SD_logging("testfile.log", "a");
-  init_display("Logging...");
-  */
-
+  // State machines
   on_off_state_t brake_state = OFF;
   on_off_state_t proximity_state = OFF;
   on_off_state_t turn_state = OFF;
 
+  // Set variables
+  float velocity = 0;
+
   // Loop forever
   while (1) {
-    // sample analog inputs
-    //double x_acc, y_acc, z_acc;
-    //sample_accel(&x_acc, &y_acc, &z_acc);
-
-    // display results
-    //printf("x: %.5fg\ty: %.5fg\tz:%.5fg\n", x_acc, y_acc, z_acc);
-    //char buf[16] = {0};
-    //snprintf(buf, 16, "%lf", x_acc);
-    //display_write(buf, 0);
-
-    // log to SD card
-    /*
-    char x_buf[10];
-    char y_buf[10];
-    char z_buf[10];
-    gcvt(x_acc, 10, x_buf);
-    gcvt(y_acc, 10, y_buf);
-    gcvt(z_acc, 10, z_buf);
-    simple_logger_log("x:%.10sg, y: %.10sg, z: %.10sg\n", x_buf, y_buf, z_buf);
-    printf("Wrote line to SD card\n");*/
+    // Determines sampling rate
+    nrf_delay_ms(10);
 
     for (int i=0; i<3; i++) {
       nrf_gpio_pin_toggle(LEDS[i]);
     }
 
-    // Determines sampling rate
-    nrf_delay_ms(1000);
-  
+    // GET MEASUREMENTS AND INPUTS
+    float x_acc, y_acc, z_acc;
+    sample_9250_accelerometer(&x_acc, &y_acc, &z_acc);
+    // TODO: set threshold as macro
+    bool turned_left = (y_acc > 0.3), turned_right = (y_acc < -0.3);
+    // Temporary debugging for turn signal debugging (button press instead of ble button)
+    bool ble_left;
+    if (button_press_time > 0) {
+      ble_left = !gpio_read(28);
+      button_press_time = 0;
+    }
+    printf("timer: %i\n", button_press_time);
+    bool ble_right = false;
+    bool left_turn, right_turn = false;
+
+    // TODO: Implement hall-effect sensors to update velocity
+    // Used to check left_turn and right_turn (i.e. tilted left/right && velocity > 0);
+    //printf("X: %f, Y: %f, Z: %f\n", x_acc, y_acc, z_acc);
+    //velocity = velocity + x_acc * 0.01;
+    //printf("Velocity: %f\n", velocity);
+
     // STATE MACHINES
     // State machine for brake
     switch(brake_state) {
@@ -250,7 +304,7 @@ int main (void) {
         break;
       }
     }
-    
+
     // State machine for proximity sensors
     switch(proximity_state) {
       case OFF: {
@@ -260,17 +314,59 @@ int main (void) {
         break;
       }
     }
-    
+
     // State machine for turn indicators
     switch(turn_state) {
       case OFF: {
+        if (ble_left) {
+          turn_state = LEFT;
+          left_turn = true;
+        } else if (ble_right) {
+          turn_state = RIGHT;
+          right_turn = true;
+        } else {
+          turn_state = OFF;
+          printf("IN STATE OFF\n");
+        }
         break;
       }
-      case ON: {
+      case LEFT: {
+        if (ble_left || turn_time_on > 60 || turned_left) {
+          turn_state = OFF;
+          turn_time_on = 0;
+        } else if (ble_right) {
+          turn_state = RIGHT;
+          right_turn = true;
+        } else {
+          turn_state = LEFT;
+          left_turn = true;
+          turn_time_on++;
+          printf("IN STATE LEFT\n");
+        }
+        break;
+      }
+      case RIGHT: {
+        if (ble_right || turn_time_on > 60 || turned_right) {
+          turn_state = OFF;
+          turn_time_on = 0;
+        } else if (ble_left) {
+          turn_state = LEFT;
+          left_turn = true;
+        } else {
+          turn_state = RIGHT;
+          right_turn = true;
+          turn_time_on++;
+          printf("IN STATE RIGHT\n");
+        }
         break;
       }
     }
+
+    // Handle turn_state outputs
+    if (left_turn) {
+      // turn on left turn lights
+    } else if (right_turn) {
+      // turn on right turn lights
+    }
   }
 }
-
-
