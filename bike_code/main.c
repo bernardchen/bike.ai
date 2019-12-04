427082c9f8db484b61eebbdaac7d300a99502a57
Colhodm <arjunmishra@berkeley.edu>
2019-12-03 17:10:11 -0800

    Working LED stuff

--- bike_code/main.c
+++ bike_code/main.c
@@ -1,251 +1,74 @@
-// Non-intrusive bicycle lighting system
-// includes brake, turn, and blind-spot lighting
 
-// C includes
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <math.h>
-#include <time.h>
-
-// nrf includes
+#include <string.h>
+#include "nrf_drv_pwm.h"
+#include "app_util_platform.h"
 #include "app_error.h"
-#include "nrf.h"
+#include "boards.h"
+#include "bsp.h"
+#include "nrf_drv_clock.h"
 #include "nrf_delay.h"
-#include "nrf_gpio.h"
-#include "nrf_log.h"
-#include "nrf_log_ctrl.h"
-#include "nrf_log_default_backends.h"
-#include "nrf_pwr_mgmt.h"
-#include "nrf_serial.h"
-#include "nrfx_gpiote.h"
-#include "nrfx_saadc.h"
 
-// buckler includes
-#include "buckler.h"
-#include "display.h"
 
-// macros for accelerometer
-#define pi acos(-1.0)
-#define radToDeg (180.0 / pi)
-// system_bias = 2.885 / 3.0
-#define lsb_size ((0.6 * 6) / 4096)
-#define slope (.420 * 2.885 / 3.0) // .420 * system_bias
-#define bias (-1.5 * 2.885 / 3.0) // -1.5 * system_bias
+#define OUTPUT_PIN 27
 
-// ADC channels
-#define X_CHANNEL 0
-#define Y_CHANNEL 1
-#define Z_CHANNEL 2
+static nrf_drv_pwm_t m_pwm0 = NRF_DRV_PWM_INSTANCE(0);
 
-// Bucker LED array
-static uint8_t LEDS[3] = {BUCKLER_LED0, BUCKLER_LED1, BUCKLER_LED2};
+// Declare variables holding PWM sequence values. In this example only one channel is used 
+nrf_pwm_values_individual_t seq_values[] = {
+13,13,13,13,13,13,13,13,
+13,13,13,13,13,13,13,13,
+6,6,6,6,6,6,6,6,
+0,0
+};
+nrf_pwm_sequence_t const seq =
+{
+    .values.p_individual = seq_values,
+    .length          = NRF_PWM_VALUES_LENGTH(seq_values),
+    .repeats         = 0,
+    .end_delay       = 0
+};
 
-// callback for SAADC events
-void saadc_callback (nrfx_saadc_evt_t const * p_event) {
-  // don't care about adc callbacks
+
+// Set duty cycle between 0 and 100%
+void pwm_update_duty_cycle(uint8_t duty_cycle)
+{
+    nrf_drv_pwm_simple_playback(&m_pwm0, &seq, 1, NRF_DRV_PWM_FLAG_LOOP);
 }
 
-// sample a particular analog channel in blocking mode
-nrf_saadc_value_t sample_value (uint8_t channel) {
-  nrf_saadc_value_t val;
-  ret_code_t error_code = nrfx_saadc_sample_convert(channel, &val);
-  APP_ERROR_CHECK(error_code);
-  return val;
+static void pwm_init(void)
+{
+    nrf_drv_pwm_config_t const config0 =
+    {
+        .output_pins =
+        {
+            OUTPUT_PIN, // channel 0
+        },
+        .base_clock   = NRF_PWM_CLK_16MHz,
+        .count_mode   = NRF_PWM_MODE_UP,
+        .top_value    = 20,
+        .load_mode    = NRF_PWM_LOAD_INDIVIDUAL,
+        .step_mode    = NRF_PWM_STEP_AUTO
+    };
+    // Init PWM without error handler
+    APP_ERROR_CHECK(nrf_drv_pwm_init(&m_pwm0, &config0, NULL));
+    
 }
 
-// BUCKER/nRF INITIALIZATION CODE
 
-void init_RTT() {
-  ret_code_t error_code = NRF_SUCCESS;
-
-  // initialize RTT library
-  error_code = NRF_LOG_INIT(NULL);
-  APP_ERROR_CHECK(error_code);
-  NRF_LOG_DEFAULT_BACKENDS_INIT();
-
-  printf("RTT initialized!\n");
-}
-
-void init_accelerometer() {
-  ret_code_t error_code = NRF_SUCCESS;
-
-  // initialize analog to digital converter
-  nrfx_saadc_config_t saadc_config = NRFX_SAADC_DEFAULT_CONFIG;
-  saadc_config.resolution = NRF_SAADC_RESOLUTION_12BIT;
-  error_code = nrfx_saadc_init(&saadc_config, saadc_callback);
-  APP_ERROR_CHECK(error_code);
-
-  // initialize analog inputs
-  // configure with 0 as input pin for now
-  nrf_saadc_channel_config_t channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(0);
-  channel_config.gain = NRF_SAADC_GAIN1_6; // input gain of 1/6 Volts/Volt, multiply incoming signal by (1/6)
-  channel_config.reference = NRF_SAADC_REFERENCE_INTERNAL; // 0.6 Volt reference, input after gain can be 0 to 0.6 Volts
-
-  // specify input pin and initialize that ADC channel for X, Y, and Z
-  channel_config.pin_p = BUCKLER_ANALOG_ACCEL_X;
-  error_code = nrfx_saadc_channel_init(X_CHANNEL, &channel_config);
-  APP_ERROR_CHECK(error_code);
-  channel_config.pin_p = BUCKLER_ANALOG_ACCEL_Y;
-  error_code = nrfx_saadc_channel_init(Y_CHANNEL, &channel_config);
-  APP_ERROR_CHECK(error_code);
-  channel_config.pin_p = BUCKLER_ANALOG_ACCEL_Z;
-  error_code = nrfx_saadc_channel_init(Z_CHANNEL, &channel_config);
-  APP_ERROR_CHECK(error_code);
-
-  printf("Accelerometer initialized!\n");
-}
-
-void init_SD_logging() {
-  ret_code_t error_code = NRF_SUCCESS;
-
-  // Enable SoftDevice (used to get RTC running)
-  nrf_sdh_enable_request();
-
-  // Initialize GPIO driver
-  if (!nrfx_gpiote_is_init()) {
-    error_code = nrfx_gpiote_init();
-  }
-  APP_ERROR_CHECK(error_code);
-
-  // Configure GPIOs
-  nrf_gpio_cfg_output(BUCKLER_SD_ENABLE);
-  nrf_gpio_cfg_output(BUCKLER_SD_CS);
-  nrf_gpio_cfg_output(BUCKLER_SD_MOSI);
-  nrf_gpio_cfg_output(BUCKLER_SD_SCLK);
-  nrf_gpio_cfg_input(BUCKLER_SD_MISO, NRF_GPIO_PIN_NOPULL);
-
-  nrf_gpio_pin_set(BUCKLER_SD_ENABLE);
-  nrf_gpio_pin_set(BUCKLER_SD_CS);
-
-  // Initialize SD card
-  const char filename[] = "testfile.log";
-  const char permissions[] = "a"; // w = write, a = append
-
-  // Start file
-  simple_logger_init(filename, permissions);
-
-  printf("SD card logging initialized!\n");
-}
-
-void init_display(char* message) {
-  ret_code_t error_code = NRF_SUCCESS;
-
-  nrf_drv_spi_t spi_instance = NRF_DRV_SPI_INSTANCE(1);
-  nrf_drv_spi_config_t spi_config = {
-    .sck_pin = BUCKLER_LCD_SCLK,
-    .mosi_pin = BUCKLER_LCD_MOSI,
-    .miso_pin = BUCKLER_LCD_MISO,
-    .ss_pin = BUCKLER_LCD_CS,
-    .irq_priority = NRFX_SPI_DEFAULT_CONFIG_IRQ_PRIORITY,
-    .orc = 0,
-    .frequency = NRF_DRV_SPI_FREQ_4M,
-    .mode = NRF_DRV_SPI_MODE_2,
-    .bit_order = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST
-  };
-  error_code = nrf_drv_spi_init(&spi_instance, &spi_config, NULL, NULL);
-  APP_ERROR_CHECK(error_code);
-  display_init(&spi_instance);
-
-  display_write(message, DISPLAY_LINE_0);
-
-  printf("Display initialized!\n");
-}
-
-void init_buckler_LEDs() {
-  ret_code_t error_code = NRF_SUCCESS;
-  nrf_gpio_cfg_output(LEDS[3]);
-
-  // initialize GPIO driver
-  if (!nrfx_gpiote_is_init()) {
-    error_code = nrfx_gpiote_init();
-  }
-  APP_ERROR_CHECK(error_code);
-
-  // configure leds
-  // manually-controlled (simple) output, initially set
-  nrfx_gpiote_out_config_t out_config = NRFX_GPIOTE_CONFIG_OUT_SIMPLE(true);
-  for (int i=0; i<3; i++) {
-    error_code = nrfx_gpiote_out_init(LEDS[i], &out_config);
-    APP_ERROR_CHECK(error_code);
-  }
-}
-
-void sample_accel(double* x_acc, double* y_acc, double* z_acc) {
-  nrf_saadc_value_t x_val = sample_value(X_CHANNEL);
-  nrf_saadc_value_t y_val = sample_value(Y_CHANNEL);
-  nrf_saadc_value_t z_val = sample_value(Z_CHANNEL);
-
-  *x_acc = (x_val * lsb_size + bias) / slope;
-  *y_acc = (y_val * lsb_size + bias) / slope;
-  *z_acc = (z_val * lsb_size + bias) / slope;
-
-  // Used to calculate angles of rotation
-  //double theta = atan(x_acc / pow(pow(y_acc, 2) + pow(z_acc, 2), 0.5)) * radToDeg;
-  //double psi = atan(y_acc / pow(pow(x_acc, 2) + pow(z_acc, 2), 0.5)) * radToDeg;
-  //double phi = atan(z_acc / pow(pow(y_acc, 2) + pow(x_acc, 2), 0.5)) * radToDeg;
-}
-
-typedef enum {
-  RIGHT,
-  LEFT,
-  OFF,
-  ON
-} on_off_state_t;
-
-int main (void) {
-  // Initialization code
-  init_RTT();
-  init_accelerometer();
-  init_buckler_LEDs();
-  printf("Buckler initialized!\n");
-
-  // State machines
-  on_off_state_t brake_state = OFF;
-  on_off_state_t proximity_state = OFF;
-  on_off_state_t turn_state = OFF;
-
-  // Loop forever
-  while (1) {
-    // Determines sampling rate
-    nrf_delay_ms(10);
-
-    for (int i=0; i<3; i++) {
-      nrf_gpio_pin_toggle(LEDS[i]);
+int main(void)
+{
+    // Start clock for accurate frequencies
+    NRF_CLOCK->TASKS_HFCLKSTART = 1; 
+    // Wait for clock to start
+    while(NRF_CLOCK->EVENTS_HFCLKSTARTED == 0) 
+        ;
+    pwm_init();
+    pwm_update_duty_cycle(0);
+    while(1)
+    {
     }
-
-    // STATE MACHINES
-    // State machine for brake
-    switch(brake_state) {
-      case OFF: {
-        break;
-      }
-      case ON: {
-        break;
-      }
-    }
-
-    // State machine for proximity sensors
-    switch(proximity_state) {
-      case OFF: {
-        break;
-      }
-      case ON: {
-        break;
-      }
-    }
-
-    // State machine for turn indicators
-    switch(turn_state) {
-      case OFF: {
-        break;
-      }
-      case LEFT: {
-        break;
-      }
-      case RIGHT: {
-        break;
-      }
-    }
-  }
 }
+
+
+/** @} */
