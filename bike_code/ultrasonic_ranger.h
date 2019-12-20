e2b7f3cbb336d72296a51e26576e1c8464885396
Michael Duong <mduong15@berkeley.edu>
2019-12-19 18:59:41 -0800

    Revise comments for ultrasonic_ranger.c and .h

--- bike_code/ultrasonic_ranger.h
+++ bike_code/ultrasonic_ranger.h
@@ -1,7 +1,7 @@
 // Wrapper for the Grove Ultrasonic Ranger
 //
 // Uses a state machine to step through the process 
-// presented gy Grove to read the distance.
+// presented by Grove to read the distance.
 // Example codes can be seen on the official website:
 // http://wiki.seeedstudio.com/Grove-Ultrasonic_Ranger/
 #pragma once
@@ -15,7 +15,8 @@
 #include "buckler.h"
 
 
-// the different states for the ultrasonic ranger
+// The different states for the ultrasonic ranger when measuring.
+// Not that relevant for the uesr.
 typedef enum {
 	SWITCH_TO_OUTPUT,
 	FIRST_LOW,
@@ -30,20 +31,29 @@ typedef enum {
 
 
 // enum to determine which Buckler port is used
-// for the ultrasonic ranger
-typedef enum {
+// for the ultrasonic ranger.
+// Set to UNUSED in init_ultrasonic_ranger if the corresponding ranger is not being used.
+	// Always will use GroveD0 or GroveA0, not 1.
+typedef enum {S
 	A,
 	D,
 	UNUSED
 } buckler_port_t;
 
+// Call the function to initialize the ultrasonic ranger.
+// Input:
+// left_port is the port for the left ultrasonic ranger
+// right_port is the port for the right ultrasonic ranger
+	// use the UNUSED enum if the ranger is not b eing used
+// initLEDs is whether or not to init the hardcoded LEDs too
+	// 0 is don't init, any other value is
 void init_ultrasonic_ranger(buckler_port_t left_port, buckler_port_t right_port, uint32_t initLEDs);
 
 // These functions will return the distance measured by the ultrasonic ranger.
 // It will go through the entire process of switching it to output, and
 // then switching to input, and do all the calculations.
 // Will take 12 millisecond max to run per side. Timeout set to that to avoid taking too long to poll
-// This means in standard polling of both sides, the delay will be about 30 milliseconds.
+// This means in standard polling of both sides, the delay will be about 30 milliseconds (about 15 per side).
 // Distance can be measured up to a max of 200cm.
 // Any range greater than that will be considered 600 (timeout)
 // It may also be 600 if for some reason, there is a timeout
